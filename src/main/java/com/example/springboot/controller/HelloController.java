040cb7cfc75df678a0820dc873fcbb8967525876
Guillermo <gsemx@outlook.com>
2021-12-14 17:26:58 -0600

    Update HelloController.java

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -9,19 +9,46 @@ package com.example.springboot.controller;
 import com.example.springboot.entity.Customer;
 import com.example.springboot.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HelloController {
+    
 
     @Autowired
     private CustomerRepository customerRepository;
 
     @GetMapping("/")
-    public Iterable<Customer> index() {
-        Iterable<Customer> customer = customerRepository.findAll();
-        return customer;
+    @ResponseStatus(HttpStatus.OK)
+    public String index() {
+        return "Hola Mundo!";
     }
 
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String post() {
+        return "Hola Mundo -- object post";
+    }
+    
+    @PutMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String put() {
+        return "Hola Mundo -- object put";
+    }
+    
+    @DeleteMapping("/")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String delete() {
+        return "Hola Mundo -- object delete";
+    }
+    
+    
 }
